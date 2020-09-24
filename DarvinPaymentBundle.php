3beaf3d4e0d8954b4f30731c44819cbe7984b0d3
Igor Nikolaev <igor.sv.n@gmail.com>
2020-09-24 15:24:52 +0300

    Add missing type hint.

--- DarvinPaymentBundle.php
+++ DarvinPaymentBundle.php
@@ -16,14 +16,18 @@ use Darvin\PaymentBundle\DependencyInjection\Compiler\AddStatePass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Payment bundle
+ */
 class DarvinPaymentBundle extends Bundle
 {
     /**
      * {@inheritDoc}
      */
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
+
         $container->addCompilerPass(new AddBridgePass());
         $container->addCompilerPass(new AddGatewayNamePass());
         $container->addCompilerPass(new AddStatePass());
