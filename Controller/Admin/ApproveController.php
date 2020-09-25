199fbeb1cd9e9ee463d934687bb851c027cd56e5
Igor Nikolaev <igor.sv.n@gmail.com>
2020-09-25 10:57:51 +0300

    Refactor admin controllers.

--- Controller/Admin/ApproveController.php
+++ Controller/Admin/ApproveController.php
@@ -18,14 +18,14 @@ use Darvin\PaymentBundle\Workflow\Transitions;
 use Darvin\Utils\Flash\FlashNotifierInterface;
 use Darvin\Utils\HttpFoundation\AjaxResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
- * Approve controller
+ * Approve admin controller
  */
 class ApproveController extends AbstractController
 {
@@ -43,51 +43,6 @@ class ApproveController extends AbstractController
      * @var \Darvin\Utils\Flash\FlashNotifierInterface
      */
     private $flashNotifier;
-
-    /**
-     * @var \Symfony\Component\HttpFoundation\RequestStack
-     */
-    private $requestStack;
-
-    /**
-     * @param string $token Payment token
-     *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
-     */
-    public function __invoke(string $token): Response
-    {
-        $payment = $this->getPaymentByToken($token);
-        $request = $this->requestStack->getCurrentRequest();
-
-        $this->validatePayment($payment, Transitions::APPROVE);
-
-        if (!$this->authorizationChecker->isGranted(Permission::EDIT, $payment)) {
-            throw new AccessDeniedException(
-                sprintf('You do not have "%s" permission on "%s" class objects.', Permission::EDIT, get_class($payment))
-            );
-        }
-
-        $referer = $request->headers->get(
-            'referer',
-            $this->adminRouter->generate($payment, Payment::class, AdminRouterInterface::TYPE_INDEX, [], UrlGeneratorInterface::ABSOLUTE_URL)
-        );
-
-        $this->workflow->apply($payment, Transitions::APPROVE);
-        $this->em->flush();
-
-        $successMessage = 'payment.action.approve.success';
-
-        if ($request->isXmlHttpRequest()) {
-            return new AjaxResponse(null, true, $successMessage, [], $referer);
-        }
-
-        $this->flashNotifier->success($successMessage);
-
-        return new RedirectResponse($referer);
-    }
 
     /**
      * @param \Darvin\AdminBundle\Route\AdminRouterInterface $adminRouter Admin router
@@ -114,10 +69,41 @@ class ApproveController extends AbstractController
     }
 
     /**
-     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack Request stack
+     * @param \Symfony\Component\HttpFoundation\Request $request Request
+     * @param string                                    $token   Payment token
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
      */
-    public function setRequestStack(RequestStack $requestStack): void
+    public function __invoke(Request $request, string $token): Response
     {
-        $this->requestStack = $requestStack;
+        $payment = $this->getPaymentByToken($token);
+
+        $this->validatePayment($payment, Transitions::APPROVE);
+
+        if (!$this->authorizationChecker->isGranted(Permission::EDIT, $payment)) {
+            throw new AccessDeniedException(
+                sprintf('You do not have "%s" permission on "%s" class objects.', Permission::EDIT, get_class($payment))
+            );
+        }
+
+        $referer = $request->headers->get(
+            'referer',
+            $this->adminRouter->generate($payment, Payment::class, AdminRouterInterface::TYPE_INDEX, [], UrlGeneratorInterface::ABSOLUTE_URL)
+        );
+
+        $this->workflow->apply($payment, Transitions::APPROVE);
+
+        $this->em->flush();
+
+        $successMessage = 'payment.action.approve.success';
+
+        if ($request->isXmlHttpRequest()) {
+            return new AjaxResponse(null, true, $successMessage, [], $referer);
+        }
+
+        $this->flashNotifier->success($successMessage);
+
+        return new RedirectResponse($referer);
     }
 }
--- Controller/Admin/OperationTrait.php
+++ Controller/Admin/OperationTrait.php
@@ -18,9 +18,7 @@ use Darvin\Utils\Flash\FlashNotifierInterface;
 use Darvin\Utils\HttpFoundation\AjaxResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -41,36 +39,62 @@ trait OperationTrait
     protected $authorizationChecker;
 
     /**
-     * @var \Darvin\PaymentBundle\Form\Renderer\AbstractFormRenderer
-     */
-    protected $formRenderer;
-
-    /**
      * @var \Darvin\Utils\Flash\FlashNotifierInterface
      */
     protected $flashNotifier;
 
     /**
-     * @var \Symfony\Component\HttpFoundation\RequestStack
+     * @var \Darvin\PaymentBundle\Form\Renderer\AbstractFormRenderer
      */
-    protected $requestStack;
+    protected $formRenderer;
 
     /**
-     * @param string $method     Payment method
-     * @param string $transition Payment transition
-     * @param string $token      Payment token
+     * @param \Darvin\AdminBundle\Route\AdminRouterInterface $adminRouter Admin router
+     */
+    public function setAdminRouter(AdminRouterInterface $adminRouter): void
+    {
+        $this->adminRouter = $adminRouter;
+    }
+
+    /**
+     * @param \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $authorizationChecker Authorization checker
+     */
+    public function setAuthorizationChecker(AuthorizationCheckerInterface $authorizationChecker): void
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    /**
+     * @param \Darvin\Utils\Flash\FlashNotifierInterface $flashNotifier Flash notifier
+     */
+    public function setFlashNotifier(FlashNotifierInterface $flashNotifier): void
+    {
+        $this->flashNotifier = $flashNotifier;
+    }
+
+    /**
+     * @param \Darvin\PaymentBundle\Form\Renderer\AbstractFormRenderer $formRenderer View widget pool
+     */
+    public function setFormRenderer(AbstractFormRenderer $formRenderer): void
+    {
+        $this->formRenderer = $formRenderer;
+    }
+
+    /**
+     * @param string                                    $method     Payment method
+     * @param string                                    $transition Payment transition
+     * @param string                                    $token      Payment token
+     * @param \Symfony\Component\HttpFoundation\Request $request    Request
      *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     * @return \Symfony\Component\HttpFoundation\Response
      * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
      */
-    public function execute(string $method, string $transition, string $token): Response
+    public function execute(string $method, string $transition, string $token, Request $request): Response
     {
         $payment = $this->getPaymentByToken($token);
+
         $bridge  = $this->getBridge($payment->getGateway());
         $gateway = $this->getGateway($payment->getGateway());
-        $request = $this->getRequest();
 
         if (!$this->authorizationChecker->isGranted(Permission::EDIT, $payment)) {
             throw new AccessDeniedException(
@@ -91,6 +115,7 @@ trait OperationTrait
 
         if ($response->isSuccessful()) {
             $this->workflow->apply($payment, $transition);
+
             $this->em->flush();
 
             if (parse_url($referer, PHP_URL_PATH) === parse_url($redirectUrl, PHP_URL_PATH)) {
@@ -114,7 +139,9 @@ trait OperationTrait
                 '%code%'    => $response->getCode(),
                 '%message%' => $response->getMessage(),
             ], 'payment_event'),
-            ['payment' => $payment]
+            [
+                'payment' => $payment,
+            ]
         );
 
         $errorMessage = sprintf('Payment server response: %s', $response->getMessage());
@@ -126,59 +153,5 @@ trait OperationTrait
         }
 
         return new RedirectResponse($referer);
-    }
-
-    /**
-     * @param \Darvin\AdminBundle\Route\AdminRouterInterface $adminRouter Admin router
-     */
-    public function setAdminRouter(AdminRouterInterface $adminRouter): void
-    {
-        $this->adminRouter = $adminRouter;
-    }
-
-    /**
-     * @param \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $authorizationChecker Authorization checker
-     */
-    public function setAuthorizationChecker(AuthorizationCheckerInterface $authorizationChecker): void
-    {
-        $this->authorizationChecker = $authorizationChecker;
-    }
-
-    /**
-     * @param \Darvin\PaymentBundle\Form\Renderer\AbstractFormRenderer $formRenderer View widget pool
-     */
-    public function setFormRenderer(AbstractFormRenderer $formRenderer): void
-    {
-        $this->formRenderer = $formRenderer;
-    }
-
-    /**
-     * @param \Darvin\Utils\Flash\FlashNotifierInterface $flashNotifier Flash notifier
-     */
-    public function setFlashNotifier(FlashNotifierInterface $flashNotifier): void
-    {
-        $this->flashNotifier = $flashNotifier;
-    }
-
-    /**
-     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack Request stack
-     */
-    public function setRequestStack(RequestStack $requestStack): void
-    {
-        $this->requestStack = $requestStack;
-    }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\Request
-     *
-     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-     */
-    private function getRequest(): Request
-    {
-        if (null === $this->requestStack->getCurrentRequest()) {
-            throw new NotFoundHttpException();
-        }
-
-        return $this->requestStack->getCurrentRequest();
     }
 }
